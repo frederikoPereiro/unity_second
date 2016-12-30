7f02d4e022b4022a684b5caa159a35fc755d13a7
ivo shet <ivan.v.shet@gmail.com>
2016-12-31 01:13:45 +0700

    the exercise with a different function

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -4,13 +4,24 @@ using UnityEngine;
 
 public class Hero : MonoBehaviour {
 
+	private int a = 5;
+	private int b = 6;
+
 	// Use this for initialization
 	void Start () {
-		
+		print (myFunction (a, b));
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+	int myFunction(int first, int second)
+	{
+		return first+second;
+	}
+
+
+
 }
