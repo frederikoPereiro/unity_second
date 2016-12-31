3ff4b0c3670787310663083d6cf61af6da7e45c3
ivo shet <ivan.v.shet@gmail.com>
2017-01-01 02:52:17 +0700

    add some method in instead class

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -4,11 +4,18 @@ using UnityEngine;
 
 public class Hero : MonoBehaviour {
 
-	Vector3 positionOfBucket;
+	private Vector3 positionOfBucket;
+	private int firstElement = 0;
+	private int secondElement = 0;
+	private int thirdElement = 0;
+	private int value = 5;
 
 	// Use this for initialization
 	void Start () {
-
+		InsteadClass firstObject = new InsteadClass (value);
+		firstObject.a = 1;
+		int a = firstObject.getValue ();
+		firstObject.print (a);
 	}
 
 	// Update is called once per frame
@@ -17,7 +24,46 @@ public class Hero : MonoBehaviour {
 		this.transform.position = positionOfBucket;
 	}
 
+	private void FunctionOne()
+	{
+		if(secondElement > firstElement)
+		{
+			thirdElement = secondElement + firstElement;
+			Debug.Log ("element = " + thirdElement);
+		}
+		else
+		{
+			Debug.Log (firstElement);
+		}
+	}
+
+	private class InsteadClass
+	{
+		public int a;
+		private int b;
+
+		//the constructor
+		public InsteadClass(int a)
+		{
+			setValue(a);
+		}
+
+		private void setValue(int value)
+		{
+			b = value;
+		}
+		public int getValue()
+		{
+			return b;
+		}
+		//the method
+		public void print(int a)
+		{
+			string logMessage = "variable is " + a;
+			Debug.Log (logMessage);
+		}
 
 
+	}
 
 }
