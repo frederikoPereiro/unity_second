c7f4334e0ebe69c8fce1c10be6c9d0a1ecbd81dc
ivo shet <ivan.v.shet@gmail.com>
2016-12-31 21:04:59 +0700

    remove other functions from the first lesson

--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -4,14 +4,11 @@ using UnityEngine;
 
 public class Hero : MonoBehaviour {
 
-//	private int a = 5;
-//	private int b = 6;
-
 	Vector3 positionOfBucket;
 
 	// Use this for initialization
 	void Start () {
-		
+
 	}
 
 	// Update is called once per frame
@@ -20,18 +17,7 @@ public class Hero : MonoBehaviour {
 		this.transform.position = positionOfBucket;
 	}
 
-	private int myFunction(int first, int second)
-	{
-		return first+second;
-	}
-		
-	private void secondFunction()
-	{
-		int i = 0;
-		while(i<10)
-		{
-			print ("i = " + i);
-			i++;
-		}
-	}
+
+
+
 }
