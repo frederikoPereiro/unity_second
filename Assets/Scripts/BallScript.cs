b786fc0eded32222d9c12527414f872a9882a575
ivo shet <ivan.v.shet@gmail.com>
2017-01-06 01:20:07 +0700

    remove drop and bucket, added stars and paddle

---
+++ Assets/Scripts/BallScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallScript : MonoBehaviour
+{
+	public PaddleScript paddle;
+	private bool gameStarted = false;
+	private Vector3 paddleVector;
+
+
+	// Use this for initialization
+	void Start ()
+	{
+		paddleVector = this.transform.position - paddle.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if(!gameStarted)
+		{
+			this.transform.position = paddle.transform.position + paddleVector;
+			if(Input.GetMouseButtonDown(0))
+			{
+				gameStarted = true;
+				this.GetComponent<Rigidbody2D> ().velocity = new Vector3 (2f, 2f);
+			}
+		}
+	
+	}
+}
+
---
+++ Assets/Scripts/PaddleScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 paddleVector = new Vector3 (0.0f, this.transform.position.y, 0);
+		float mousePositionX = Input.mousePosition.x / Screen.width * 10;
+		paddleVector.x = Mathf.Clamp (mousePositionX, -4.0f, 4f);
+		this.transform.position = paddleVector;
+		
+	}
+}
