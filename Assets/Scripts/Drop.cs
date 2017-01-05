3bc74fb5b15ebe4b08107057f8b126136cabc148
ivo shet <ivan.v.shet@gmail.com>
2017-01-05 19:43:57 +0700

    add drop class

---
+++ Assets/Scripts/Drop.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Drop : MonoBehaviour
+{
+	public Hero hero;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+	void OnCollisionEnter2D(Collision2D col)
+	{
+		Destroy (gameObject);
+	}
+}
+
--- Assets/Scripts/Hero.cs
+++ Assets/Scripts/Hero.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Hero : MonoBehaviour {
-	//create game object
+
+	private const float Z_POSITION = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -14,15 +15,13 @@ public class Hero : MonoBehaviour {
 	void Update () {
 		moveBucket ();
 
-
 	}
-
+	//move the bucket
 	private void moveBucket()
 	{
-		Vector3 positionBucket = new Vector3 (this.transform.position.x, this.transform.position.y, 0);
-		float mouseCurrentPositionX = Input.mousePosition.x / Screen.width * 10;
-		positionBucket.x = Mathf.Clamp (mouseCurrentPositionX, -4.24f, 4.24f);
+		Vector3 positionBucket = new Vector3 (-0.62f, this.transform.position.y, Z_POSITION);
+		float currentMouseX = Input.mousePosition.x / Screen.width * 10;
+		positionBucket.x = Mathf.Clamp (currentMouseX, -4.31f, 4.34f);
 		this.transform.position = positionBucket;
 	}
-		
 }
